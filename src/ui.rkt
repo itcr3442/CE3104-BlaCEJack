b97442d5d95bde0f03dac62b93dec175d1b07f61
Alejandro Soto <alejandro@34project.org>
2021-03-11 17:19:45 -0600

    Show a text field for each player name

--- src/ui.rkt
+++ src/ui.rkt
@@ -9,6 +9,32 @@
   (cond [(not (integer? X)) (raise "Expected an integer numbe rof players")]
         [(or (< X 1) (> X 3)) (raise "There may only be one, two, or three players")])
 
-  (let* ([names-dialog (new dialog% [label "Player nams"])])
-    (new button% [parent names-dialog] [label "Play"])
+  (let*
+    ([names-dialog (new dialog% [label "Player nams"])]
+     [name-fields (add-name-fields names-dialog X 1 empty)])
+
+    (new button%
+         [parent names-dialog]
+         [label "Play"]
+         [callback (λ (button event)
+                      (let ([player-names
+                              (map (λ (field) (string-trim (send field get-value)))
+                                   name-fields)])
+
+                        (cond [(empty? (filter (compose not non-empty-string?) player-names))
+                               (send names-dialog show #f)
+                               (raise "To do")])))])
+
     (send names-dialog show #t)))
+
+(define (add-name-fields dialog up-to next fields)
+  (cond [(> next up-to) (reverse fields)]
+        [else (add-name-fields
+                dialog
+                up-to
+                (+ next 1)
+                (cons (new text-field%
+                           [parent dialog]
+                           [label
+                             (string-append "Player #"
+                                            (number->string next))]) fields))]))
